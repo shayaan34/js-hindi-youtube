1f543c76d0e06c39b62ff820e0e3243f0eb65d71
shayaan34 <shayaankhan1453@gmail.com>
2025-01-12 01:01:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("hello world")
+
+let box = 'fruits';
+console.log(box); // fruits
+
+box = 'atta';
+
+
+let a = 3;
+a = 'hi';
+console.log(a);
+
+const person = {
+    firstName: 'shayaan',
+    lastName: 'khan',
+    fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+};
+console.log(person.fullName());
+
+const accountid = 14455
+let accountEmail = "shayaankhan1453@Ggmail.com"
+var accountPassword = "LENOVO#240487"
+
+ accountEmail = "shayaankhan14532gmail.com"
+ accountPassword = "Lenovo#240487"
+ accountcity = "mumbai"
+
+ console.table([accountid,accountEmail,accountPassword,accountcity])
+
+
